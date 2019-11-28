219b771c80e387e94524db373399670ccc0c5fc9
Mark <zhouganqing2018@gmail.com>
2019-11-28 08:49:47 +0800

    Update wsgi.py

--- LabProject/LabProject/wsgi.py
+++ LabProject/LabProject/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from os.path import join, dirname, abspath
 from django.core.wsgi import get_wsgi_application
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LabProject.settings')
 
